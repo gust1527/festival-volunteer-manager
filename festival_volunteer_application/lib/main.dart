7c9f00425fe562f5dcaf1f481dfbf1974bf30306
Sexyfoley <alundoe@gmail.com>
2024-02-05 21:59:42 +0100

    Added AppBar with title

--- festival_volunteer_application/lib/main.dart
+++ festival_volunteer_application/lib/main.dart
@@ -9,8 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title:  const Text("ØF' '24"),
+          backgroundColor: Colors.green[600],
+        ),
         body: Center(
           child: Text('Hello World!'),
         ),
