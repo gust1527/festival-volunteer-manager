049ed0ab72cb877155296c93fa67ffd94a2508ab
gust1527 <gustavemilholmsimonsen@gmail.com>
2024-04-09 23:33:46 +0200

    Added firebase dependencies and started work on Google Sign In.
    Having issues with the client-id not being registered with my localhost server

--- festival_volunteer_application/lib/Services/auth.dart
+++ festival_volunteer_application/lib/Services/auth.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 class AuthService{
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // Initialize GoogleSignIn with client ID
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: "16043473379-k1oqol4v6lk9hnd69sb91ecf52mdm1fo.apps.googleusercontent.com",
+  );
 
   // sign out
   Future<void> signOut() async {
@@ -15,22 +18,20 @@ class AuthService{
 
   // google sign in
   Future<void> googleLogin() async {
-      try {
-          final googleUser = await GoogleSignIn().signIn();
+    try {
+      final googleUser = await _googleSignIn.signIn();
 
-          if (googleUser == null) return;
+      if (googleUser == null) return;
 
-        final googleAuth = await googleUser.authentication;
-        final authCredential = GoogleAuthProvider.credential(
-            accessToken: googleAuth.accessToken,
-            idToken: googleAuth.idToken,
-        );
+      final googleAuth = await googleUser.authentication;
+      final authCredential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-        await FirebaseAuth.instance.signInWithCredential(authCredential);
-      } on FirebaseAuthException catch (e) {
-          print(e.message);
-      }
+      await FirebaseAuth.instance.signInWithCredential(authCredential);
+    } on FirebaseAuthException catch (e) {
+      print(e.message);
     }
+  }
 }
-
-   
--- festival_volunteer_application/lib/main.dart
+++ festival_volunteer_application/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:festival_volunteer_application/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
