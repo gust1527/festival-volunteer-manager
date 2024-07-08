4bbdcf8fbb8e50bb0b949e1e41894229c86fb483
gust1527 <gustavemilholmsimonsen@gmail.com>
2024-07-08 12:11:33 +0200

    More mergin with tjanse-long-description

--- festival_volunteer_application/lib/Pages/HomeScreen.dart
+++ festival_volunteer_application/lib/Pages/HomeScreen.dart
@@ -46,24 +46,11 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> _initializeCalendarEvents() async {
-<<<<<<< HEAD
-    await _gcalProvider.loadCredentials();
-=======
->>>>>>> main
     setState(() {
       _relevantEvents = _gcalProvider.getFutureEvents('8e87aa9e93550d69001740ad7e4d8a7f85e7ded096626424c8494a8da9e7ea68@group.calendar.google.com');
       _musicEvents = _gcalProvider.getFutureEvents('o8et2jfhroob27aoa5op6dud2k@group.calendar.google.com');
     });
   }
-<<<<<<< HEAD
-
-  @override
-  void dispose() {
-    _gcalProvider.close();
-    super.dispose();
-  }
-=======
->>>>>>> main
 
   @override
   Widget build(BuildContext context) {
@@ -81,17 +68,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     if (!snapshot.hasData) {
                       return const Center(child: CircularProgressIndicator());
                     } else {
-<<<<<<< HEAD
-                      DateTime dateTime = snapshot.requireData.time.toDate();
-                      String formattedTime = DateFormat('EEEE d. MMMM @ HH:mm', 'da_DK').format(dateTime);
-                      return Expanded(
-                          flex: 1,
-                          child: ExpandedDialogTile(
-                            title: 'Din tjans',
-                            content: 'Du har fået tjansen "${snapshot.requireData.name}", som indebærer at "${snapshot.requireData.shortDescription}". Du skal møde til tjansen "$formattedTime"',
-                            route: '/tjanser',
-                          ));
-=======
                       try {
                         DateTime dateTime = snapshot.requireData.time.toDate();
                         String formattedTime = DateFormat('EEEE d. MMMM @ HH:mm', 'da_DK').format(dateTime);
@@ -105,7 +81,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       } catch (e) {
                         return const Center(child: Text('Error loading tjans'));
                       }
->>>>>>> main
                     }
                   },
                 ),
@@ -113,26 +88,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   future: _relevantEvents,
                   builder: (BuildContext context, AsyncSnapshot<List<calendar.Event>> snapshot) {
                     if (!snapshot.hasData) {
-<<<<<<< HEAD
-                      return const Center(child: CircularProgressIndicator());
-                    } else {
-                      // Fetch the first event from the list
-                      calendar.Event firstEvent = snapshot.data!.first;
-
-                      // Fetch the start time of the first event
-                      DateTime startTime = firstEvent.start!.dateTime!.toLocal();
-                      String formattedTime = DateFormat('HH:mm').format(startTime);
-
-                      // Fetch the summary of the first event
-                      String summary = firstEvent.summary ?? 'Ingen kommende begivenheder';
-                      return Expanded(
-                          flex: 1,
-                          child: ExpandedDialogTile(
-                            title: 'Relevant information',
-                            content: snapshot.data!.isNotEmpty ? 'Næste begivenhed: $summary @ $formattedTime' : 'Ingen kommende begivenheder',
-                            route: '/information',
-                          ));
-=======
                       return Center(child: Text('No calendar data found for activities events: ${snapshot.error}'));
                     } else if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Center(child: CircularProgressIndicator());
@@ -157,7 +112,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       } catch (e) {
                         return const Center(child: Text('Error loading relevant information'));
                       }
->>>>>>> main
                     }
                   },
                 ),
@@ -165,36 +119,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   future: _musicEvents,
                   builder: (BuildContext context, AsyncSnapshot<List<calendar.Event>> snapshot) {
                     if (!snapshot.hasData) {
-<<<<<<< HEAD
-                      return const Center(child: CircularProgressIndicator());
-                    } else {
-                      // Fetch the first event from the list
-                      calendar.Event firstMusicEvent = snapshot.data!.first;
-
-                      // Fetch the start time of the first event
-                      DateTime startTime = firstMusicEvent.start!.dateTime!.toLocal();
-                      String formattedTime = DateFormat('HH:mm').format(startTime);
-
-                      // Fetch the summary of the first event
-                      String summary = firstMusicEvent.summary ?? 'Ingen kommende artister';
-                      return Expanded(
-                          child: Row(
-                        children: <Widget>[
-                          Expanded(
-                              child: ExpandedDialogTile(
-                            title: 'Musik program',
-                            content: snapshot.data!.isNotEmpty ? 'Næste artist: $summary @ $formattedTime' : 'Ingen kommende artister',
-                            route: '/music',
-                          )),
-                          Expanded(
-                              child: ExpandedDialogTile(
-                            title: 'Madboder',
-                            content: 'Du har fået tjansen , som indebærer . Du skal møde til tjansen kl ',
-                            route: '/foodAndBeverages',
-                          )),
-                        ],
-                      ));
-=======
                       return Center(child: Text('No calendar data found for music events: ${snapshot.error}'));
                     } else if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Center(child: CircularProgressIndicator());
@@ -229,7 +153,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       } catch (e) {
                         return const Center(child: Text('Error loading music program'));
                       }
->>>>>>> main
                     }
                   },
                 ),
--- festival_volunteer_application/lib/Providers/gcal_provider.dart
+++ festival_volunteer_application/lib/Providers/gcal_provider.dart
@@ -1,43 +1,4 @@
 import 'dart:convert';
-<<<<<<< HEAD
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:googleapis/calendar/v3.dart' as calendar;
-import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart' as http;
-
-class GCALProvider {
-  final _scopes = [calendar.CalendarApi.calendarReadonlyScope];
-  late ServiceAccountCredentials _credentials;
-  AuthClient? _client;
-
-  GCALProvider();
-
-  Future<void> loadCredentials() async {
-    // Load the service account credentials from the JSON file
-    String jsonString = await rootBundle.loadString('oedsted-festival-app-74186dc80a46.json');
-    Map<String, dynamic> jsonMap = json.decode(jsonString);
-    _credentials = ServiceAccountCredentials.fromJson(jsonMap);
-    await _authenticate();
-  }
-
-  Future<void> _authenticate() async {
-    _client = await clientViaServiceAccount(_credentials, _scopes);
-  }
-
-  Future<List<calendar.Event>> getFutureEvents(String calendarId) async {
-    if (_client == null) {
-      throw Exception("Client not authenticated");
-    }
-
-    var calendarApi = calendar.CalendarApi(_client!);
-    var now = DateTime.now().toUtc();
-    var eventList = await calendarApi.events.list(
-      calendarId,
-      timeMin: now,
-      singleEvents: true,
-      orderBy: 'startTime',
-    );
-=======
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:googleapis/calendar/v3.dart' as calendar;
 import 'package:http/http.dart' as http;
@@ -60,17 +21,9 @@ class GCALProvider {
 
     var data = json.decode(response.body);
     var eventList = calendar.Events.fromJson(data);
->>>>>>> main
 
     print(eventList.items);
 
     return eventList.items ?? [];
-<<<<<<< HEAD
-  }
-
-  void close() {
-    _client?.close();
-=======
->>>>>>> main
   }
 }
